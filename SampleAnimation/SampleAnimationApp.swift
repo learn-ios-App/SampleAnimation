f73e0de6e36e36a4ac5d412cedf65d79312371be
Aoitoridayo <s2071150@s.konan-u.ac.jp>
2023-05-10 23:08:28 +0900

    Initial Commit

---
+++ SampleAnimation/SampleAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleAnimationApp.swift
+//  SampleAnimation
+//
+//  Created by 渡邊魁優 on 2023/05/10.
+//
+
+import SwiftUI
+
+@main
+struct SampleAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
